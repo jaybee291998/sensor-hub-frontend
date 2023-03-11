1f6ee30af28e55f7a4d4987a6e12060c6eefdc2d
jayvee-pearlpay <jaybee291998@gmail.com>
2023-03-11 17:45:49 +0800

    Update t1.js

--- js/t1.js
+++ js/t1.js
@@ -1,3 +1,5 @@
 alert("hello");
 let body = document.getElementsByTagName("BODY")["0"];
 body.style.backgroundColor = "red";
+let add_student_btn = document.getElementById("new_opt");
+add_student_btn.disabled = true;
