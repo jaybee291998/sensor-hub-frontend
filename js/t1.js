0af98bad7bbaf39b94fe428431d40a4d51a68325
jayvee-pearlpay <jaybee291998@gmail.com>
2023-03-13 14:24:42 +0800

    Update t1.js

--- js/t1.js
+++ js/t1.js
@@ -1,13 +1,13 @@
-alert("hello");
-let body = document.getElementsByTagName("BODY")["0"];
-let h1 = document.createElement("h1");
-h1.innerHTML = "HELOOO WOLDS";
-body.appendChild(h1);
-body.style.backgroundColor = "red";
-let add_student_btn = document.getElementById("new_opt");
-add_student_btn.disabled = true;
+// alert("hello");
+// let body = document.getElementsByTagName("BODY")["0"];
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "HELOOO WOLDS";
+// body.appendChild(h1);
+// body.style.backgroundColor = "red";
+// let add_student_btn = document.getElementById("new_opt");
+// add_student_btn.disabled = true;
 
 
 
-let b = document.querySelector('[data-id="91"]');
-b.disabled = true;
+// let b = document.querySelector('[data-id="91"]');
+// b.disabled = true;
